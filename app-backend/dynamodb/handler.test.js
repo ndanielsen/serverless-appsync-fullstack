e408f150c18ded9e4a6595a21c2d1ed0ff130aa9
Nathan Danielsen <nathan.danielsen@gmail.com>
2019-11-28 10:58:20 -0800

    updates

--- app-backend/dynamodb/handler.test.js
+++
@@ -1,5 +0,0 @@
-import { graphqlHandler } from './handler';
-
-it('graphqlHandler should be a function', () => {
-  expect(typeof graphqlHandler).toBe('function');
-});
--- app-backend/rds/handler.test.js
+++
@@ -1,9 +0,0 @@
-import { graphqlHandler, playgroundHandler } from './handler';
-
-it('graphqlHandler should be a function', () => {
-  expect(typeof graphqlHandler).toBe('function');
-});
-
-it('playgroundHandler should be a function', () => {
-  expect(typeof playgroundHandler).toBe('function');
-});
